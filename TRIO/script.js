e22c72597fc36cb58b454b03fe98c1f7dc90b1ef
themysterysolver <arjunprabhakar1910@gmail.com>
2025-05-16 13:43:56 +0530

    FEAT:dat and gui

--- TRIO/script.js
+++ TRIO/script.js
@@ -6,37 +6,29 @@ const h=window.innerHeight;
 const w=window.innerWidth;
 const render=new THREE.WebGLRenderer({antialias:true});
 render.setSize(w,h);
+render.setClearColor(0xffffad)
 document.body.appendChild(render.domElement);
 
 //camera
 const fov=75;
 const aspect=w/h;
 const near=1;
-const far=100;
+const far=1000;
 const camera=new THREE.PerspectiveCamera(fov,aspect,near,far);
-camera.position.z=10;
+camera.position.z=150;
 
 //scene
 const scene=new THREE.Scene();
 
 //logic
-const geo=new THREE.ConeGeometry(2,10,10); //radius,ht,radial segment
-const material=new THREE.MeshStandardMaterial({color:0xffff00,flatShading:true});
-const cone=new THREE.Mesh(geo,material)
-scene.add(cone);
-
-const wireMat=new THREE.MeshBasicMaterial({
-    color: 0xffffff,wireframe:true
-})
-const wireMesh=new THREE.Mesh(geo,wireMat);
-cone.add(wireMesh);
-
-const light = new THREE.HemisphereLight(
-    new THREE.Color(1, 0.5, 0), 
-    new THREE.Color(0.2, 0, 0.2),  
-    1
-);
-scene.add( light );
+const gui=new dat.GUI();
+const params={count:100,opacity:1.0} //this decides the value
+gui.add(params,'count',10,2000,1).onChange((val)=>{
+  console.log("count",val);
+}); //obj,key to the param(property),min,max,step
+gui.add(params,'opacity',0,1,0.01).onChange((val)=>{
+  console.log("opacity",val);
+});
 
 //contols
 const control=new OrbitControls(camera,render.domElement);
